bc3c848a80601100be8958dfe10af0432edebc09
Bob Lannon <bob@new-host-6.home>
2017-02-20 10:20:01 -0500

    symlinks script

---
+++ install_scripts/make_symlinks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ln -s $HOME/cfg/dotfiles/profile/.bash_aliases $HOME/.bash_aliases
+ln -s $HOME/cfg/dotfiles/profile/.bash_profile $HOME/.bash_profile
+ln -s $HOME/cfg/dotfiles/profile/.bashrc $HOME/.bashrc
+rm $HOME/Library/Preferences/com.googlecode.iterm2.plist
+ln -s $HOME/cfg/dotfiles/profile/com.googlecode.iterm2.plist $HOME/Library/Preferences/com.googlecode.iterm2.plist
+ln -s $HOME/cfg/dotfiles/git/.gitconfig $HOME/.gitconfig
+ln -s $HOME/cfg/dotfiles/git/.gitignore $HOME/.gitignore
