1a4186383444626381ca52fcefeeb93b95011c62
Bob Lannon <bob.lannon@mapbox.com>
2017-05-19 20:24:28 +0000

    add jshintrc

--- install_scripts/make_symlinks.sh
+++ install_scripts/make_symlinks.sh
@@ -2,6 +2,7 @@
 ln -s $HOME/cfg/dotfiles/profile/.bash_aliases $HOME/.bash_aliases
 ln -s $HOME/cfg/dotfiles/profile/.bash_profile $HOME/.bash_profile
 ln -s $HOME/cfg/dotfiles/profile/.bashrc $HOME/.bashrc
+ln -s $HOME/cfg/dotfiles/profile/.jshintrc $HOME/.jshintrc
 rm $HOME/Library/Preferences/com.googlecode.iterm2.plist
 ln -s $HOME/cfg/dotfiles/profile/com.googlecode.iterm2.plist $HOME/Library/Preferences/com.googlecode.iterm2.plist
 ln -s $HOME/cfg/dotfiles/git/.gitconfig $HOME/.gitconfig
