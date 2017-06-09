a56693936ef30e74970f2956583fac8b980b1a14
Bob Lannon <bob.lannon@mapbox.com>
2017-06-09 20:08:12 +0000

    add new tmux build to ubu_up

--- install_scripts/ubu_up.sh
+++ install_scripts/ubu_up.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-apt-get -y -q install bash-completion ack-grep htop tree vim-nox ruby-dev rake memcached
+apt-get -y -q install bash-completion ack-grep htop tree vim-nox ruby-dev rake memcached libevent-dev libncurses5-dev libncursesw5-dev ncdu
 
 pip install awscli --upgrade
+
+# build and install new tmux
+mkdir src
+cd src
+wget https://github.com/tmux/tmux/releases/download/2.3/tmux-2.3.tar.gz
+tar xvzf tmux-2.3.tar.gz
+cd tmux-2.3/
+./configure && make
+make install
+
