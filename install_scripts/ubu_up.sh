e8a91c5a29c21786fe8e877aeed1484954f5d6e1
Bob Lannon <bob.lannon@mapbox.com>
2019-12-03 17:43:32 -0500

    might not need to build tmux anymore

--- install_scripts/ubu_up.sh
+++ install_scripts/ubu_up.sh
@@ -20,11 +20,11 @@ yarn global add jshint
 yarn global add jsonlint
 
 # build and install new tmux
-mkdir src
-cd src
-wget https://github.com/tmux/tmux/releases/download/2.3/tmux-2.3.tar.gz
-tar xvzf tmux-2.3.tar.gz
-cd tmux-2.3/
-./configure && make
-make install
+# mkdir src
+# cd src
+# wget https://github.com/tmux/tmux/releases/download/2.3/tmux-2.3.tar.gz
+# tar xvzf tmux-2.3.tar.gz
+# cd tmux-2.3/
+# ./configure && make
+# make install
 
